e87e03851a6c17da69b4017ccda0f98e4c4f8323
javguerra <javguerra>
2021-07-02 04:14:18 +0200

    select bug

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -3,6 +3,6 @@
 <select name="{{$name}}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
     <option value="0"><span class="text-gray-400">{{__('Choose an option')}}</span></option>
     @foreach($options as $option)
-    <option value="{{$option->id}}" {{old($name) == $obtion->id || $objId == $option->id ? 'selected' : '' }}>{{$option->name}}</option>
+    <option value="{{$option->id}}" {{old($name) == $option->id || $objId == $option->id ? 'selected' : '' }}>{{$option->name}}</option>
     @endforeach
 </select>
