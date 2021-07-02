a264ced51a07c9aa1d9a59070a453aaed1e59c46
javguerra <javguerra>
2021-07-02 10:41:59 +0200

    select bug

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,4 +1,4 @@
-@props(['disabled' => false, 'options', 'name', 'objId' ?? ''])
+@props(['disabled' => false, 'options', 'name', 'objId' => ''])
 
 <select name="{{$name}}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
     <option value="0"><span class="text-gray-400">{{__('Choose an option')}}</span></option>
